3327135562c867de8dd0b56b397975cd2b800abc
dylancrossan231 <45007106+dylancrossan231@users.noreply.github.com>
2019-11-14 13:51:39 +0000

    add grocery search from API

--- resources/views/calories/apiview.blade.php
+++ resources/views/calories/apiview.blade.php
@@ -57,6 +57,41 @@ use App\Api;
 
 
 
+<?php
+
+  //This line creates a new Client from guzzle -> This client is basically a http request
+  $client1 = new Client();
+
+  //This part querys a url that I have hardcoded, you will want to make this a variable in yours
+
+  //The headers part is a header that is attached to your http request - The tesco api expects a subscription key
+  $response =$client->request('GET', 'https://dev.tescolabs.com/grocery/products/?query="milk"&offset=0&limit=10', [
+      'headers' => [
+          'Ocp-Apim-Subscription-Key' => 'a3c51c289be148ac9492336f4b6dadff'
+          //env('API_KEY')
+          
+          
+      ]
+  ]);
+
+  //This function runs your request
+  $response=$response->getBody();
+  //This line turns the returned json into an array -> you need to do this so laravel can understand the json object
+  $response = json_decode($response, true);
+   // dd($response);
+
+  ?>
+
+
+     @foreach($response['uk']['ghs']['products']['results'] as $manyproducts)
+     <p> products: {{$manyproducts['tpnb']}}</p>
+
+
+
+     @endforeach
+
+
+
 
 
 
