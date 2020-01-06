6d8b475dc29df75f7423d0f345ca214e1576a468
dylancrossan231 <45007106+dylancrossan231@users.noreply.github.com>
2020-01-06 16:41:24 +0000

    fixed max length key

--- resources/views/calories/apisearchshowproduct.blade.php
+++ resources/views/calories/apisearchshowproduct.blade.php
@@ -6,20 +6,14 @@
         <h2>
         Product:
       </h2> {{ $product['description'] }}
-             <p>GTIN:{{ $product['gtin'] }}</p>
              <p>TPNB:{{ $product['tpnb'] }}</p>
-             <p>TPNC:{{ $product['tpnc'] }}</p>
-             <p>BRAND:{{ $product['brand'] }}</p>
              <h2>
             Macronutrients
            </h2>
              @foreach($product['calcNutrition']['calcNutrients'] as $nutritional)
 
-               <p>Nutritional Info  :  {{ $nutritional['name']}}g</p>
-               <p>Per 100g  :  {{ $nutritional['valuePer100']}}g</p>
-
-
-                
+               <p>Nutritional Info  :  {{ $nutritional['name']}}</p>
+               <p>Per 100g  :  {{ $nutritional['valuePer100'][1]}}g</p>
 
              @endforeach
     @endforeach
